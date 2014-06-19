a4c91c752edb16ddc66ff8082e8ddd6be6a1f623
Jude Aakjaer <santouras@gmail.com>
2014-06-19 05:25:31 +0000

    create notes based on private_message topics
    puts in a "Save" button on private_messages which will save this
    topic through to a private note.

---
+++ assets/javascripts/create_profile_notes_button.js
@@ -0,0 +1,28 @@
+Discourse.AlertButton = Discourse.ButtonView.extend({
+  text: 'Save',
+  title: 'Save this conversation to a User Note',
+
+  click: function() {
+    this.set('loading', true);
+    Discourse.ajax("/create_profile_notes/add", {
+      type: "POST",
+      data: {topic_id: this.get('controller.content.id')}
+    }).then(function(){
+      this.set('loading', false);
+      this.set('text', 'Saved');
+      this.rerender();
+    }.bind(this));
+  },
+
+  renderIcon: function(buffer) {
+    buffer.push("<i class='fa fa-comment'></i>");
+  }
+});
+
+Discourse.TopicFooterButtonsView.reopen({
+  addAlertButton: function() {
+    if (this.get('controller.currentUser.staff') && this.get('controller.content.archetype') == 'private_message') {
+      this.attachViewClass(Discourse.AlertButton);
+    }
+  }.on("additionalButtons")
+});
---
+++ plugin.rb
@@ -0,0 +1,59 @@
+# name: create-profile-note
+# about: copy a topic into a note
+# authors: Jude Aakjaer
+
+after_initialize do
+  module ::CreateProfileNotesPlugin
+    class Engine < ::Rails::Engine
+      engine_name "create_profile_notes_plugin"
+      isolate_namespace CreateProfileNotesPlugin
+    end
+
+    class CreateProfileNotesController < ActionController::Base
+      include CurrentUser
+      before_filter :ensure_staff_user
+
+      def add
+        if params[:topic_id].nil?
+          render status: 400, json: false
+          return
+        end
+
+        topic = ::Topic.where(id: params[:topic_id], archetype: :private_message).first
+
+        if topic.nil?
+          render_forbidden
+          return
+        end
+
+        notes.add_note(topic.posts.first.raw, false, {topic_id: params[:topic_id]})
+        render status: :ok, json: false
+      end
+
+      private
+
+      def render_forbidden
+        render status: :forbidden, json: false
+      end
+
+      def ensure_staff_user
+        render_forbidden if !current_user.staff?
+      end
+
+      def notes
+        @_notes ||= ProfileNotesPlugin::ProfileNotes.new(current_user, current_user)
+      end
+    end
+  end
+
+  CreateProfileNotesPlugin::Engine.routes.draw do
+    post '/add' => 'create_profile_notes#add'
+  end
+
+  Discourse::Application.routes.append do
+    mount ::CreateProfileNotesPlugin::Engine, at: '/create_profile_notes'
+  end
+end
+
+# Register UI Components
+register_asset "javascripts/create_profile_notes_button.js"
